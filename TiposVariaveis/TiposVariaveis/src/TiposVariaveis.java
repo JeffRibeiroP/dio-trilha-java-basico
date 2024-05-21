1c1a009c5aefc6f31ba63e8cd9d872ee54853bc2
JeffRibeiro <jeffribeirop@gmail.com>
2024-05-21 10:27:38 -0300

    aprendendo tipos de variaveis em java com Dio

---
+++ TiposVariaveis/TiposVariaveis/src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+        int numero = 0;
+        numero = 100;
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(numero);
+        System.out.println(VALOR_DE_PI);
+    }
+}
